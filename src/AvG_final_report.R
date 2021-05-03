6192b4bac52b5b5e090365d4bd7c276cce22ebee
Sebastian Vaca <svaca@broadinstitute.org>
2021-05-03 09:20:57 -0400

    Updated final report function

--- src/AvG_final_report.R
+++ src/AvG_final_report.R
@@ -1,5 +1,3 @@
-#.libPaths(c( "C:/Users/svaca/Documents/R/win-library/3.5" , "C:/Program Files/R/R-3.5.2/library"))
-
 library(dplyr)
 library(tidyr)
 library(stringr)
@@ -14,39 +12,29 @@ change_names<-function(data){
 }
 
 args <- commandArgs(TRUE)
-#print(args)
 
+# load arguments
 params_file <- as.character(args[1])
-avg_results_path_zip <- as.character(args[2])
+avg_results_path <- as.character(args[2])
 MetaData_Analytes_path <- as.character(args[3])
 Transition_Locator <- as.character(args[4])
 MetaData_Replicate <- as.character(args[5])
-output_path <- as.character(args[6])
-#success_file <- as.character(args[7])
+MetaData_PrecursorResults <- as.character(args[6])
+output_path <- as.character(args[7])
+# success_file <- as.character(args[8])
 
-
-# params_file <- "C:/Users/Sebastian Vaca/PycharmProjects/Hardvard_Ext/Project/AvG_Example_only10/AvG_Params.R"
-# avg_results_path<- "C:/Users/Sebastian Vaca/PycharmProjects/Hardvard_Ext/pset_3_cookiecutter/2019sp-final-project-SebVaca/data/avg_results/"
-# MetaData_Analytes<- fread("C:/Users/Sebastian Vaca/PycharmProjects/Hardvard_Ext/pset_3_cookiecutter/2019sp-final-project-SebVaca/data/avg_results/ID_Analyte_glossary_3_2a535757.csv",
-#                           stringsAsFactors = F) %>% change_names()
-# Transition_Locator<- fread("C:/Users/Sebastian Vaca/PycharmProjects/Hardvard_Ext/pset_3_cookiecutter/2019sp-final-project-SebVaca/data/csv_ds/ID_transition_locator.csv",
-#                           stringsAsFactors = F) %>% change_names()
-# MetaData_Replicate<- fread("C:/Users/Sebastian Vaca/PycharmProjects/Hardvard_Ext/pset_3_cookiecutter/2019sp-final-project-SebVaca/data/csv_ds/ID_Rep.csv",
-#                            stringsAsFactors = F) %>% change_names()
-# output_path<- "C:/Users/Sebastian Vaca/PycharmProjects/Hardvard_Ext/pset_3_cookiecutter/2019sp-final-project-SebVaca/data/final_result/"
-
+# read files
 MetaData_Analytes<- fread(MetaData_Analytes_path, stringsAsFactors = F) %>% change_names()
 Transition_Locator<- fread(Transition_Locator, stringsAsFactors = F) %>% change_names()
 MetaData_Replicate<- fread(MetaData_Replicate, stringsAsFactors = F) %>% change_names()
+MetaData_PrecursorResults<- fread(MetaData_PrecursorResults, stringsAsFactors = F) %>% change_names()
 
 avg_results_path_zip_list = unlist(strsplit(avg_results_path_zip, " "))
-#print(avg_results_path_zip_list)
 
 for (file in avg_results_path_zip_list){
 	unzip(zipfile = file)
 }
 avg_results_path = "avg_results"
-#list.files(avg_results_path)
 
 source(params_file)
 ## Peak_BOundaries
@@ -98,8 +86,85 @@ if(length(ListFiles_Transitions)>=1){
   write.table(Trans_results,file=file.path(output_path,"Transition_results.csv"),quote=F,row.names=F,col.names=T,sep=",")
 }  
 
+###########NEW
+## Report Replicates
+  RepList<-list.files(avg_results_path,pattern = paste0("Report_GR_Replicate_"))
+  ListFiles_Replicates<-paste0(avg_results_path,"/",RepList)
+  if(length(RepList)>=1){
+    l_rep <- lapply(ListFiles_Replicates, fread, header = F,sep=';', stringsAsFactors = FALSE)
+    NewReplicates_All_Results <- rbindlist( l_rep )
+    colnames(NewReplicates_All_Results)<-c("ID_Analyte","IsotopeLabelType" , "ID_Rep",  "Similarity.Score" , "MPRA.Score" , "Library.dotp",  "Intensity.Score", "Score.MassError", "Comment")
+
+    NewReplicates_All_Results<-NewReplicates_All_Results %>%
+      mutate(ID_Analyte=as.character(ID_Analyte),
+             ID_Rep=as.character(ID_Rep)) %>%
+      left_join(MetaData_Analytes, by = c("ID_Analyte")) %>%
+      left_join(MetaData_Replicate, by = c("ID_Rep"))
+
+    Exponents<-c(9.5,4.5,2.5,0.5)
+    NewReplicates_All_Results<-NewReplicates_All_Results %>%
+      mutate(Skor=Similarity.Score^Exponents[1]*Library.dotp^Exponents[2]*Score.MassError^Exponents[3]*MPRA.Score^Exponents[4])
+
+    write.table(NewReplicates_All_Results,file=file.path(output_path,"BeforeOpt_Replicates.csv"),quote=F,row.names=F,col.names=T,sep=",")
+  }
+
+  ## Report ReScore
+  ReScoreList<-list.files(avg_results_path,pattern = paste0("Report_GR_ReScore_"))
+  ListFiles_ReScore<-paste0(avg_results_path,"/",ReScoreList)
+  if(length(ReScoreList)>=1){
+    l_reScore <- lapply(ListFiles_ReScore, fread, header = F,sep=';', stringsAsFactors = FALSE)
+    NewReScore_All_Results <- rbindlist(l_reScore)
+    colnames(NewReScore_All_Results)<-c("ID_Analyte","IsotopeLabelType" , "ID_Rep",  "Similarity.Score" , "MPRA.Score" , "Library.dotp",  "Intensity.Score", "Score.MassError", "Comment")
+
+    NewReScore_All_Results<-NewReScore_All_Results %>%
+      mutate(ID_Analyte=as.character(ID_Analyte),
+             ID_Rep=as.character(ID_Rep)) %>%
+      left_join(MetaData_Analytes, by = c("ID_Analyte")) %>%
+      left_join(MetaData_Replicate, by = c("ID_Rep"))
+
+    Exponents<-c(9.5,4.5,2.5,0.5)
+    NewReScore_All_Results<- NewReScore_All_Results %>%
+      mutate(Skor=Similarity.Score^Exponents[1]*Library.dotp^Exponents[2]*Score.MassError^Exponents[3]*MPRA.Score^Exponents[4])
+
+    write.table(NewReScore_All_Results,file=file.path(output_path,"AfterOpt_Replicate_Score.csv"),quote=F,row.names=F,col.names=T,sep=",")
+  }
+
+  ## Score Annotation
+
+  ReScoreList<-list.files(avg_results_path,pattern = paste0("Report_GR_ReScore_"))
+  ListFiles_ReScore<-paste0(avg_results_path,"/",ReScoreList)
+  if(length(ReScoreList)>=1){
+    l_reScore <- lapply(ListFiles_ReScore, fread, header = F,sep=';', stringsAsFactors = FALSE)
+    ScoreAnnotations <- rbindlist(l_reScore)
+    colnames(ScoreAnnotations)<-c("ID_Analyte","IsotopeLabelType" , "ID_Rep",  "Similarity.Score" , "MPRA.Score" , "Library.dotp",  "Intensity.Score", "Score.MassError", "Comment")
+
+    Exponents<-c(9.5,4.5,2.5,0.5)
+    ScoreAnnotations<-ScoreAnnotations %>%
+      mutate(ID_Analyte=as.character(ID_Analyte),
+             ID_Rep=as.character(ID_Rep)) %>%
+      left_join(MetaData_Analytes, by = c("ID_Analyte")) %>%
+      left_join(MetaData_Replicate, by = c("ID_Rep")) %>%
+      select(IsotopeLabelType, ProteinName, PeptideModifiedSequence,PrecursorCharge,IsDecoy, FileName,Similarity.Score, MPRA.Score, Library.dotp,Score.MassError) %>%
+      mutate(Skor=Similarity.Score^Exponents[1]*Library.dotp^Exponents[2]*Score.MassError^Exponents[3]*MPRA.Score^Exponents[4])
+
+    Annotations_PrecursorResults<-MetaData_PrecursorResults %>%
+      left_join(ScoreAnnotations, by = c("IsotopeLabelType", "ProteinName", "PeptideModifiedSequence", "PrecursorCharge", "IsDecoy", "FileName"))%>%
+      select(PrecursorResultLocator,
+             Similarity.Score,MPRA.Score,Library.dotp,Score.MassError,Skor)%>%
+      mutate(Similarity.Score=ifelse(is.na(Similarity.Score),"#N/A",Similarity.Score),
+             MPRA.Score=ifelse(is.na(MPRA.Score),"#N/A",MPRA.Score),
+             Library.dotp=ifelse(is.na(Library.dotp),"#N/A",Library.dotp),
+             Score.MassError=ifelse(is.na(Score.MassError),"#N/A",Score.MassError),
+             Skor=ifelse(is.na(Skor),"#N/A",Skor)) %>%
+      rename(ElementLocator=PrecursorResultLocator,
+             annotation_AvG_Similarity_Score=Similarity.Score,
+             annotation_AvG_MPRA_Score=MPRA.Score,
+             annotation_AvG_SpectralLibSim_Score=Library.dotp,
+             annotation_AvG_MassError_Score=Score.MassError,
+             annotation_AvG_Score=Skor)
+
+    fwrite(Annotations_PrecursorResults,file=file.path(output_path,"AnnotationsPrecursorResults.csv"), sep=",", row.names=F)
+  }
+
 print(output_path)
 print(list.files(output_path))
-
-#write.table(
-  #fread(MetaData_Analytes_path, stringsAsFactors = F), file = success_file, quote=F,row.names=F,col.names=T,sep=",")
